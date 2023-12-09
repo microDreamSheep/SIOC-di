2bf057bd01d80038e220f04665ecad54314e70d2
microDreamSheep <atthemc313128460@gmail.com>
2023-12-09 22:39:22 +0800

    feat: init project

---
+++ src/main/java/live/midreamsheep/frame/sioc/scan/scanner/HutoolScannerAdaptor.java
@@ -0,0 +1,19 @@
+package live.midreamsheep.frame.sioc.scan.scanner;
+
+import cn.hutool.core.util.ClassUtil;
+import live.midreamsheep.frame.sioc.scan.inter.ClassesAbstractScanner;
+import lombok.AllArgsConstructor;
+
+import java.util.Set;
+
+@AllArgsConstructor
+public class HutoolScannerAdaptor extends ClassesAbstractScanner {
+
+    private String basePackage;
+
+    @Override
+    public Set<Class<?>> doScan() {
+        //扫描指定目录下的所有jar包
+        return ClassUtil.scanPackage(basePackage);
+    }
+}
